c7192572ef12054fc84ea2aa2fb146533d8b54e1
Angelo Fallaria <ba.fallaria@gmail.com>
2021-10-23 12:55:48 +0800

    Finally finish the game() function

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-// Simulate 5 games of Rock Paper Scissors
+// Simulate 5 rounds of Rock Paper Scissors
 function game() {
   let playerSelection;
   let computerSelection;
@@ -99,21 +99,39 @@ function game() {
 
   console.log("Rock Paper Scissors vs AI! Win 3 rounds out of 5 to win!")
 
+  // Play 5 rounds
   for (let i = 0; i < 5; i++) {
+    console.log(`ROUND ${i + 1}`);
 
     // Get player input. Keep looping as long as the user keeps typing
     // stuff that isn't Rock, Paper or Scissors
     do {
-      playerSelection = prompt("Rock, paper or scissors?")
+      playerSelection = prompt(`ROUND ${i + 1}: Rock, paper or scissors?`);
     }
     while (!["ROCK", "PAPER", "SCISSORS"].includes(playerSelection.toUpperCase(), 0));
 
     // Get the computer's selection with a random algorithm
     computerSelection = computerPlay();
 
+    // Simulate a round with each participant's inputs and log the result
     roundResult = playRound(playerSelection, computerSelection);
-
     console.log(roundResult);
+
+    // Calculate the wins of the round, do nothing on tie
+    if (roundResult.includes("You win!")) {
+      playerWins++;
+    } else if (roundResult.includes("You lose!")) {
+      aiWins++;
+    }
+  }
+
+  // Declare the winner of the entire game
+  if (playerWins > aiWins) {
+    console.log("🎆 You won the game! 🎆");
+  } else if (playerWins < aiWins) {
+    console.log("😭 You lost the game! 😭");
+  } else {
+    console.log("It's a tie!");
   }
 }
 
